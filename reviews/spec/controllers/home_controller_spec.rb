bd0af84106d0bb5f710eaf096b00897747006078
Jin Cheng <jincheng88@outlook.com>
2017-07-17 16:30:44 -0500

    set up rspec

---
+++ reviews/spec/controllers/home_controller_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../rails_helper.rb'
+
+describe HomeController do
+  render_views
+  describe "GET index" do
+    it "shows a welcome message" do
+      get :index
+      expect(response.body).to include("Welcome")
+    end
+  end
+  
+end
